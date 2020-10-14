b7962ad055b5fe06a4958c5913aeed0d0fba7503
Ale <alessandrochb@gmail.com>
2020-10-14 18:51:07 +0200

    aggiunto bottone refresh

--- assets/main.js
+++ assets/main.js
@@ -2,14 +2,6 @@
 // Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
 
-// CHIEDERE I NOMI AI GIOCATORI
-
-// var player_one = prompt('nome giocatore 1');
-// console.log(player_one);
-//
-// var player_two = prompt('nome giocatore 2');
-// console.log(player_two);
-
 // CREARE I DUE TIRI DI DADI
 
 // TIRO PLAYER ONE
@@ -46,10 +38,10 @@ document.getElementById('player_two_result').innerHTML = (throws_results_player_
 
 
 if (throws_results_player_one > throws_results_player_two) {
-    document.getElementById('the_winner_is').innerHTML = ('the winner is player 1');
+    document.getElementById('the_winner_is').innerHTML = ('the winner is the player 1');
 
 } else if (throws_results_player_one < throws_results_player_two) {
-    document.getElementById('the_winner_is').innerHTML = ('the winner is player 2');
+    document.getElementById('the_winner_is').innerHTML = ('the winner is the player 2');
 } else {
     document.getElementById('the_winner_is').innerHTML = ('Draw');
 }
